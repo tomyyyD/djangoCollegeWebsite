4abbddfea8b724f36b61d0a549325544841fe738
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 11:50:10 -0500

    Update settings.py

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -126,7 +126,7 @@ PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static/query"),
+    os.path.join(PROJECT_ROOT, "static"),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
