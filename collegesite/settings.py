a5046e5eb7a15ad279474af0b76b7d3d1f2ca5f0
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 12:11:23 -0500

    heroku updates

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -125,10 +125,9 @@ USE_TZ = True
 PROJECT_ROOT   =   os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static/query/css"),
-    os.path.join(PROJECT_ROOT, "static/query/JS"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, "static"),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
