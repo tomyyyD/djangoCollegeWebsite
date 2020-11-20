b2a87e3e46a162c56c073fd4fb94066b826668c4
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 14:40:49 -0500

    Changed DB path

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'collegesite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -129,8 +129,6 @@ STATIC_URL = '/static/'
 # )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
