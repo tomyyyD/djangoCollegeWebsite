b416c6cfdd12f727feff3fd158cdd61fc6f61e99
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 15:06:10 -0500

    Update settings.py

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = ['https://git.heroku.com/college-cds-data.git']
 # Application definition
 
 INSTALLED_APPS = [
-    'query.apps.QueryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'query.apps.QueryConfig',
 ]
 
 MIDDLEWARE = [
