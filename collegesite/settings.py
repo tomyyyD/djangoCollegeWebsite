a4c1b4021b4bb7c361a7c1675f038833ba465a2f
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 14:58:40 -0500

    Update settings.py

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.miggleware.security.SecurityMiddleware',
+    'whitenoise.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
