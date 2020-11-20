ce07a12702a37542bc239c5b73835e3781f6392d
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-20 14:30:31 -0500

    Update settings.py

--- collegesite/settings.py
+++ collegesite/settings.py
@@ -122,13 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'templates/')
-STATIC_URL = '/templates/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "templates/"),
-)
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'templates/')
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, "templates/"),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
