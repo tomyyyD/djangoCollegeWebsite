eb5f5ae1a6f155548d66600e3587f66aced7ab10
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2020-11-25 19:27:03 -0500

    changed home page layout

--- query/static/query/js/details.js
+++ query/static/query/js/details.js
@@ -1,4 +1,4 @@
 const tl = gsap.timeline({defaults:{ease:'power1.out'}})
 
-tl.to('.text', {y:'0%', duration:1, stagger:0.25})
-tl.to(".intro", {y:'-100%', duration:1})
+tl.to('.text', {y:'0%', duration:1, stagger:0.1})
+tl.to(".intro", {y:'-100%', duration:0.8})
