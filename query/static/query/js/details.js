2eef2c06953c95640943761fd7d083a41ca6f0f7
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2021-01-04 10:05:01 -0500

    creating popup windows for descriptions on hover

--- query/static/query/js/details.js
+++ query/static/query/js/details.js
@@ -5,3 +5,18 @@ const tl = gsap.timeline({defaults:{ease:'power1.out'}})
 tl.to('.text', {y:'0%', duration:1, stagger:0.1})
 tl.to(".intro", {y:'-100%', duration:0.8})
 
+$(window).on('load', function(){
+    $('.dataGroup').each(function(){
+        $('.dataGroup').hover(
+            function() {
+                //console.log($(this).next())
+                //$(this).next().css('display', "flex")
+                $(this).children('.summary').css('display', "flex")
+            },
+            function(){
+                //$(this).next().css('display', "none")
+                $(this).children('.summary').css('display', "none")
+            }
+        )
+    })
+})
